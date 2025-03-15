b54e89ff764fc16b60729fe808b3d85bb6c27111
Hariish <hariishraoparanthaman@gmail.com>
2025-03-15 19:12:59 +0800

    added a way to enter and store a new student

--- file_opening_test.py
+++ file_opening_test.py
@@ -1,3 +1,9 @@
+# with open("tempfile.txt", "r") as f:
+#     content = f.read()
+#     print(content)
+
+with open("tempfile.txt", "a") as f:
+    f.write("\nBello")
+
 with open("tempfile.txt", "r") as f:
-    content = f.read()
-    print(content)
+    print(f.read())
--- test.py
+++ test.py
@@ -1,8 +1,24 @@
-'''STUDENTS_INFO = "students.txt"
+STUDENTS_INFO = "students.txt"
 COURSES_INFO = "courses.txt"
-ENROLLMENT_INFO = "enrollment.txt"'''
+ENROLLMENT_INFO = "enrollment.txt"
 
 # STUDENT CODE
+def adding_new_student(student_ID, student_name, student_contact):
+    
+    '''
+    Function: To allow new students to add their student id, name and phone number
+              Also clearly displays which is student ID, student name and student contact number in the .txt file
+    '''
+
+    with open("students.txt", "a") as STUDENTS_INFO:
+        STUDENTS_INFO.write("Student ID: " + student_ID + ", ")
+        STUDENTS_INFO.write("Student Name: " + student_name + ", ")
+        STUDENTS_INFO.write("Student Contact Number: " + student_contact + "\n")
+
+    # with open("students.txt", "r") as STUDENTS_INFO:
+    #     print(STUDENTS_INFO.read())
+
+    # return student_ID, student_name, student_contact
 
 #COURSES CODE
 
@@ -28,8 +44,13 @@ while True:
 
         # replace all print statements with a function class from either student code, courses code or enrollment code
     match userInput:
-        case 1: 
-            print("hi")
+        case 1: #adding new student
+            student_ID = input("Please enter your student ID: ")
+            student_name = input("Please enter your name: ")
+            student_contact = input("Please enter your phone number: ")
+
+            adding_new_student(student_ID, student_name,student_contact)
+
         case 2:
             print("im sad")
         case 3:
@@ -41,11 +62,7 @@ while True:
         case 6:
             print("me")
         case 7:
-            print("me")
+            break
         case _:
             print("Invalid Choice. Try Again")
         
-
-
-
-# HI This is a test
