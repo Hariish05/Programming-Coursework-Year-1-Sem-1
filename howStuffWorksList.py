3e748f960c347ec2743556cc5b0dc07f4accbdeb
Hariish <hariishraoparanthaman@gmail.com>
2025-03-18 15:30:52 +0800

    deleted unnessacry files

--- howStuffWorksList.py
+++
@@ -1,9 +0,0 @@
-# make sure file path use / not \
-'''with open("C:/Users/Janice Lee/Desktop/1 Assignments/studentInfo.txt", 'r') as f:
-    content = f.read()
-    print(content)'''
-    
-# datetime to show date
-import datetime
-x = datetime.datetime.now()
-print(x.strftime("%d %B %Y"))
